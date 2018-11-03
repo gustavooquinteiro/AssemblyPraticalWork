59144bb07adea6405134e5ba9b6c3ac9a721851b
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2018-11-03 12:52:37 -0300

    Funciona em 3/5 dos casos de teste

--- code.c
+++ code.c
@@ -14,56 +14,25 @@ int min(int a, int b){
 
 // Tá uma merda não funciona nada
 int resolve(int indice, int final, int quantidadePontes){
-
-    int caminho = 0;
+    int caminho = 10000;
+    int caminho1 = 10000;
     int estadoInicial = vetor[3*indice];
     int estadoFinal = vetor[3*indice + 1];
     int buracos = vetor[3*indice +2];
-        
-    printf("EI==%d  EF==%d BURACOS==%d FINAL==%d\n", estadoInicial, estadoFinal, buracos, final);
     
-    if (estadoFinal == final+1) {
-        /**
-         * Fulaninho chegou do outro lado do desfiladeiro
-         * retorna a quantidade de buracos
-        **/
-        printf("CHEGOU!\n");
-        return buracos; 
-    }else{
-        
-        int index = indice+1;
-        if (vetor[3*index] == estadoFinal){
-            /** 
-             * caminho sequencial: o proximo estadoInicial é igual ao estadoFinal atual, exemplo:
-             * 0 1 1
-             * 1 2 1
-             * 2 3 1 
-            **/
-            printf("CAMINHO SEQUENCIAL EI==%d EFL==%d  \n", estadoInicial, estadoFinal);
-            caminho += resolve(index, final, quantidadePontes);
-        }else{ 
-            /** tem outro caminho partindo desse indice, exemplo:
-             * 0 1 1
-             * 0 2 1
-             * 0 3 1
-             **/
-            
-            for(int i = index; i < quantidadePontes; i++){
-                if(vetor[3*i] != estadoInicial) 
-                    break;
-                printf("OUTRO CAMINHO EI==%d EF==%d  \n", estadoInicial, estadoFinal);    
-                caminho += resolve(i, final, quantidadePontes);     
-                
-            }
-            
-            
+    int i = indice+1;
+    
+    while(vetor[3*i+1] != final){
+        if (vetor[3*i] == estadoFinal){
+            caminho = buracos + vetor[3*i+2];
+        }else if (vetor[3*i] == estadoInicial ){
+            caminho1 = resolve(i, final, quantidadePontes);
         }
-        
+        i++;
     }
     
-    //imagino que irá retornar o menor valor entre dois caminhos por exemplo
-    printf("retorno\n");
-   return caminho;
+    return min(caminho,caminho1);
+   
 }
 
 
@@ -78,6 +47,6 @@ int main(){
         vetor[3*i+1] = t; // fim
         vetor[3*i+2] = b; // quantidade de buracos entre inicio e fim 
     }
-    int resultado = resolve(0, n, m);
+    int resultado = resolve(0, n+1, m);
     printf("%d\n", resultado);
 }
